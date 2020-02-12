5fd81198536567313c9fe59118a93ebcea183d1e
zmjack <jack@zmland.com>
2020-02-13 01:26:57 +0800

    v0.5.0

--- TypeSharp.Cli/Program.cs
+++ TypeSharp.Cli/Program.cs
@@ -10,12 +10,11 @@ namespace TypeSharp.Cli
     {
         public static readonly string CLI_VERSION = Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
-        public static CommandContainer CommandContainer;
+        public static CommandContainer CommandContainer = new CommandContainer(ProjectInfo.GetCurrent(), "ts");
         public static ProjectInfo ProjectInfo => CommandContainer.ProjectInfo;
 
         static void Main(string[] args)
         {
-            CommandContainer = new CommandContainer(ProjectInfo.GetCurrent(), "ts");
             CommandContainer.CacheCommands(Assembly.GetExecutingAssembly());
 
             PrintWelcome();
--- push.cmd
+++ push.cmd
@@ -1,3 +1,3 @@
-nuget push "TypeSharp/bin/Release/TypeSharp.0.4.1.nupkg" -source nuget.org
-nuget push "TypeSharp.Cli/bin/Release/dotnet-ts.0.4.1.nupkg" -source nuget.org
+nuget push "TypeSharp/bin/Release/TypeSharp.0.5.0.nupkg" -source nuget.org
+nuget push "TypeSharp.Cli/bin/Release/dotnet-ts.0.5.0.nupkg" -source nuget.org
 pause
