2cddb08f7322825a130ef1c6de2d5938127e379e
zmjack <jack@zmland.com>
2020-03-21 02:53:51 +0800

    v1.0.0

--- push.cmd
+++ push.cmd
@@ -1,3 +1,3 @@
-nuget push "TypeSharp/bin/Release/TypeSharp.0.7.0.nupkg" -source nuget.org
-nuget push "TypeSharp.Cli/bin/Release/dotnet-ts.0.7.0.nupkg" -source nuget.org
+nuget push "TypeSharp/bin/Release/TypeSharp.1.0.0.nupkg" -source nuget.org
+nuget push "TypeSharp.Cli/bin/Release/dotnet-ts.1.0.0.nupkg" -source nuget.org
 pause
