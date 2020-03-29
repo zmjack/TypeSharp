91a0de90edd6ac1c0bb2dbb380c9957c9bcb2504
zmjack <jack@zmland.com>
2020-03-29 21:47:13 +0800

    v1.0.4

--- TypeSharp/TsType.cs
+++ TypeSharp/TsType.cs
@@ -1,11 +1,67 @@
-﻿using System;
+﻿using NStandard;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace TypeSharp
 {
     public class TsType
     {
+        public TsType(CacheContainer<Type, TsType> tsTypes, Type classType = null)
+        {
+            TsProperties = new Cache<TsProperty[]>
+            {
+                CacheMethod = () =>
+                {
+                    if (classType != null)
+                    {
+                        var props = classType.GetProperties();
+                        if (classType.IsGenericType)
+                        {
+                            if (classType.IsGenericTypeDefinition)
+                            {
+                                return props.Select(prop =>
+                                {
+                                    var propType = prop.PropertyType;
+                                    if (propType.IsGenericParameter)
+                                    {
+                                        return new TsProperty
+                                        {
+                                            PropertyName = StringEx.CamelCase(prop.Name),
+                                            PropertyTypeDefinition = propType.Name,
+                                        };
+                                    }
+                                    else
+                                    {
+                                        return new TsProperty
+                                        {
+                                            PropertyName = StringEx.CamelCase(prop.Name),
+                                            PropertyType = tsTypes[propType].Value,
+                                        };
+                                    }
+                                }).ToArray();
+                            }
+                            else return new TsProperty[0];
+                        }
+                        else
+                        {
+                            return props.Select(prop =>
+                            {
+                                var propType = prop.PropertyType;
+                                return new TsProperty
+                                {
+                                    PropertyName = StringEx.CamelCase(prop.Name),
+                                    PropertyType = tsTypes[propType].Value,
+                                };
+                            }).ToArray();
+                        }
+                    }
+                    else return new TsProperty[0];
+                },
+            };
+        }
+
         /// <summary>
         /// Hint: If Namespace is null, the properties should be null.
         /// </summary>
@@ -15,9 +71,11 @@ namespace TypeSharp
 
         public TsTypeClass TypeClass { get; set; }
 
-        public TsProperty[] TsProperties { get; set; }
+        public Cache<TsProperty[]> TsProperties { get; private set; }
 
         public TsEnumValue[] TsEnumValues { get; set; }
+
+        public bool Declare { get; set; }
 
         public string ReferenceName => Namespace is null ? TypeName : $"{Namespace}.{TypeName}";
     }
--- push.cmd
+++ push.cmd
@@ -1,3 +1,3 @@
-nuget push "TypeSharp/bin/Release/TypeSharp.1.0.3.nupkg" -source nuget.org
-nuget push "TypeSharp.Cli/bin/Release/dotnet-ts.1.0.3.nupkg" -source nuget.org
+nuget push "TypeSharp/bin/Release/TypeSharp.1.0.4.nupkg" -source nuget.org
+nuget push "TypeSharp.Cli/bin/Release/dotnet-ts.1.0.4.nupkg" -source nuget.org
 pause
