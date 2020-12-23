5a91a9cc3bda1f6ea2bb2f9ca9b7300072f9017c
zmjack <jack@zmland.com>
2020-12-23 10:48:18 +0800

    v1.3.4

--- push.cmd
+++ push.cmd
@@ -1,4 +1,4 @@
-nuget push "TypeSharp/bin/Release/TypeSharp.1.3.3.nupkg" -source nuget.org
-nuget push "TypeSharp.Antd/bin/Release/TypeSharp.Antd.0.0.14.nupkg" -source nuget.org
-nuget push "TypeSharp.Cli/bin/Release/dotnet-ts.1.3.3.7.nupkg" -source nuget.org
+nuget push "TypeSharp/bin/Release/TypeSharp.1.3.4.nupkg" -source nuget.org
+nuget push "TypeSharp.Antd/bin/Release/TypeSharp.Antd.0.0.15.nupkg" -source nuget.org
+nuget push "TypeSharp.Cli/bin/Release/dotnet-ts.1.3.4.nupkg" -source nuget.org
 pause
