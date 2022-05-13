ee87512a8506cbdc31ce778377256e42bedd05b7
zmjack <jack@zmland.com>
2022-05-13 09:34:41 +0800

    v1.3.22

--- push.cmd
+++ push.cmd
@@ -1,3 +1,3 @@
-nuget push "TypeSharp/bin/Release/TypeSharp.1.3.21.nupkg" -source nuget.org
-nuget push "TypeSharp.Cli/bin/Release/dotnet-ts.6.0.4.nupkg" -source nuget.org
+nuget push "TypeSharp/bin/Release/TypeSharp.1.3.22.nupkg" -source nuget.org
+nuget push "TypeSharp.Cli/bin/Release/dotnet-ts.6.0.5.nupkg" -source nuget.org
 pause
