2da49e66b683361f869bf31261405c0f50fb2f52
Jack.Tan <195587293@qq.com>
2022-09-15 07:35:17 +0800

    v1.4.5

--- push.cmd
+++ push.cmd
@@ -1,3 +1,3 @@
 nuget push "TypeSharp/bin/Release/TypeSharp.1.4.5.nupkg" -source nuget.org
-nuget push "TypeSharp.Cli/bin/Release/dotnet-ts.6.0.11.nupkg" -source nuget.org
+nuget push "TypeSharp.Cli/bin/Release/dotnet-ts.6.0.12.nupkg" -source nuget.org
 pause
