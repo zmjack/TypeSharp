87d4b6c36487d1e29e4b7f18c3505c080304f72c
zmjack <jack@zmland.com>
2023-11-01 03:08:12 +0800

    TypeSharp.1.5.1

--- Tests/TypeSharp.Test/~ModelTests/ModelTests.DictionaryTest.cs.ts
+++ Tests/TypeSharp.Test/~ModelTests/ModelTests.DictionaryTest.cs.ts
@@ -1,9 +1,9 @@
 ﻿declare namespace TypeSharp.Test.Models {
     interface DictionaryClass {
-        dict1?: { [key: string]: number };
-        dict2?: { [key: string]: { [key: string]: number } };
-        dict3?: { [key: string]: SuperClass };
-        dict4?: { [key: string]: SpecialNsClass };
+        dict1?: { [key: string | number | symbol]: number };
+        dict2?: { [key: string | number | symbol]: { [key: string | number | symbol]: number } };
+        dict3?: { [key: string | number | symbol]: SuperClass };
+        dict4?: { [key: string | number | symbol]: SpecialNsClass };
     }
     interface SuperClass {
         nested?: SuperClass.NestedClass;
