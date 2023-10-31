1f448ab1104966dff0d291d0ae320884d661aee8
zmjack <jack@zmland.com>
2023-11-01 03:21:35 +0800

    TypeSharp.1.5.2

--- Tests/TypeSharp.Test/~ModelTests/ModelTests.DictionaryTest.cs.ts
+++ Tests/TypeSharp.Test/~ModelTests/ModelTests.DictionaryTest.cs.ts
@@ -1,9 +1,9 @@
 ﻿declare namespace TypeSharp.Test.Models {
     interface DictionaryClass {
-        dict1?: { [key: string | number | symbol]: number };
-        dict2?: { [key: string | number | symbol]: { [key: string | number | symbol]: number } };
-        dict3?: { [key: string | number | symbol]: SuperClass };
-        dict4?: { [key: string | number | symbol]: SpecialNsClass };
+        dict1?: { [key: string | number]: number };
+        dict2?: { [key: string | number]: { [key: string | number]: number } };
+        dict3?: { [key: string | number]: SuperClass };
+        dict4?: { [key: string | number]: SpecialNsClass };
     }
     interface SuperClass {
         nested?: SuperClass.NestedClass;
--- push.cmd
+++ push.cmd
@@ -1,3 +1,3 @@
-nuget push "TypeSharp/bin/Release/TypeSharp.1.5.1.nupkg" -source nuget.org
-nuget push "TypeSharp.Cli/bin/Release/dotnet-ts.7.0.1.nupkg" -source nuget.org
+nuget push "TypeSharp/bin/Release/TypeSharp.1.5.2.nupkg" -source nuget.org
+nuget push "TypeSharp.Cli/bin/Release/dotnet-ts.7.0.2.nupkg" -source nuget.org
 pause
