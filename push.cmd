9d053d0aa764c3238409fffd4225b028234817c1
zmjack <jack@zmland.com>
2023-11-01 03:09:15 +0800

    TypeSharp.1.5.1

--- push.cmd
+++ push.cmd
@@ -1,3 +1,3 @@
-nuget push "TypeSharp/bin/Release/TypeSharp.1.5.0.nupkg" -source nuget.org
-nuget push "TypeSharp.Cli/bin/Release/dotnet-ts.7.0.0.nupkg" -source nuget.org
+nuget push "TypeSharp/bin/Release/TypeSharp.1.5.1.nupkg" -source nuget.org
+nuget push "TypeSharp.Cli/bin/Release/dotnet-ts.7.0.1.nupkg" -source nuget.org
 pause
